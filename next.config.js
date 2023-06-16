f1644f998e7fcbf43f9d0fb15d62baad4849b02d
Pankaj10101 <rkay1048@gmail.com>
2023-06-16 10:21:24 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -4,13 +4,11 @@ const nextConfig = {
   experimental: {
     // Specify other experimental options here if needed
   },
+  output: 'export',
   images: {
     domains: ['rb.gy', 'image.tmdb.org'],
   },
   // Add the 'export' option to enable static HTML export
-  output: {
-    export: true,
-  },
 };
 
 module.exports = nextConfig;
